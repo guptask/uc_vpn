e607f70071ed22b334ff83f62ad9dee63d852128
Sounak Gupta <guptask@mail.uc.edu>
2015-05-18 14:38:19 -0500

    modify for other distributions and 32 bit

--- vpn_installer.sh
+++ vpn_installer.sh
@@ -1,4 +1,4 @@
-# UC VPN installation (without using Java) for 64-bit Ubuntu/Debian
+# UC VPN installation (without using Java)
 
 # Author: Sounak Gupta
 # Email : guptask@mail.uc.edu
@@ -7,9 +7,18 @@
 #!/bin/bash
 
 if test "$#" -ne 1; then
-    echo "Error! Type 'bash $0 <6+2 id>'"
+    echo "===================================================================="
+    echo "Incorrect number of arguments. Type 'bash $0 <6+2 id>'"
+    echo "===================================================================="
     exit
 fi
+
+# Install the required packages by checking the Linux distribution
+OS=$(lsb_release -si)
+ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
+VER=$(lsb_release -sr)
+sudo apt-get install libc6-i386 lib32z1 lib32nss-mdns
+#sudo yum install glibc.i686 zlib.i686 nss.i686
 
 # Delete old installation files (if present)
 sudo rm -rf ~/.juniper_networks/
@@ -24,6 +33,9 @@ mkdir -p ~/.juniper_networks/network_connect/
 # Unzip and copy the installation files
 unzip ~/Downloads/ncLinuxApp.jar -d ~/.juniper_networks/network_connect/
 
+# Delete the downloaded network connect app
+rm ~/Downloads/ncLinuxApp.jar
+
 # Change the installation directory permissions
 sudo chown root:root ~/.juniper_networks/network_connect/ncsvc
 sudo chmod 6711 ~/.juniper_networks/network_connect/ncsvc
@@ -32,9 +44,6 @@ chmod 744 ~/.juniper_networks/network_connect/ncdiag
 # Download the vpn login/logoff perl script and change its permission
 sudo wget http://homepages.uc.edu/~guptask/vpn/jnc -P /usr/local/bin/
 sudo chmod a+x /usr/local/bin/jnc
-
-# Install the required packages
-sudo apt-get install libc6-i386 lib32z1 lib32nss-mdns
 
 # Create a config directory on the installation path
 sudo mkdir -p ~/.juniper_networks/network_connect/config
@@ -54,14 +63,10 @@ suffix="/g' ~/.juniper_networks/network_connect/config/uc.conf"
 sedcmd=$prefix$username$suffix
 eval $sedcmd
 
-# Delete the downloaded network connect app
-rm ~/Downloads/ncLinuxApp.jar
-
 # Post-installation logs
 echo "====================================================================="
 echo "Installation complete. Check whether it works by logging into UC VPN."
 echo "Command for logging  in to UC VPN : jnc --nox uc"
 echo "Command for logging out of UC VPN : jnc stop"
 echo "====================================================================="
-
 
