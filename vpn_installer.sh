8e4f8416ae2d22ed1f8906f562065afa0ba00b38
Sounak Gupta <guptask@mail.uc.edu>
2015-05-18 18:27:42 -0500

    added the installer files and modified the script so that it works with an absolute path

--- vpn_installer.sh
+++ vpn_installer.sh
@@ -1,11 +1,15 @@
-# UC VPN installation (without using Java)
+#!/bin/bash
 
+###############################################################################
+#
+# UC VPN installer
 # Author: Sounak Gupta
 # Email : guptask@mail.uc.edu
 # Source: http://www.scc.kit.edu/scc/net/juniper-vpn/linux/
+#
+###############################################################################
 
-#!/bin/bash
-
+# Check the number of arguments
 if test "$#" -ne 1; then
     echo "===================================================================="
     echo "Incorrect number of arguments. Type 'bash $0 <6+2 id>'"
@@ -17,46 +21,47 @@ fi
 OS=$(lsb_release -si)
 ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
 VER=$(lsb_release -sr)
-sudo apt-get install libc6-i386 lib32z1 lib32nss-mdns
-#sudo yum install glibc.i686 zlib.i686 nss.i686
 
-# Delete old installation files (if present)
-sudo rm -rf ~/.juniper_networks/
-sudo rm -f /usr/local/bin/jnc
-
-# Download the network connect app
-wget http://homepages.uc.edu/~guptask/vpn/ncLinuxApp.jar -P ~/Downloads/
+case ${OS} in
+Debian)
+    sudo apt-get install libc6-i386 lib32z1 lib32nss-mdns
+    ;;
+*)
+    #sudo yum install glibc.i686 zlib.i686 nss.i686
+    exit
+    ;;
+esac
 
 # Create the installation path
 mkdir -p ~/.juniper_networks/network_connect/
 
-# Unzip and copy the installation files
-unzip ~/Downloads/ncLinuxApp.jar -d ~/.juniper_networks/network_connect/
+# Find the absolute path of the script
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
 
-# Delete the downloaded network connect app
-rm ~/Downloads/ncLinuxApp.jar
+# Unzip and copy the installation files
+unzip ${SCRIPTPATH}/installer_files/ncLinuxApp.jar -d ~/.juniper_networks/network_connect/
 
 # Change the installation directory permissions
 sudo chown root:root ~/.juniper_networks/network_connect/ncsvc
 sudo chmod 6711 ~/.juniper_networks/network_connect/ncsvc
 chmod 744 ~/.juniper_networks/network_connect/ncdiag
 
-# Download the vpn login/logoff perl script and change its permission
-sudo wget http://homepages.uc.edu/~guptask/vpn/jnc -P /usr/local/bin/
+# Copy the vpn login/logoff perl script and change its permission
+sudo cp ${SCRIPTPATH}/installer_files/jnc /usr/local/bin/
 sudo chmod a+x /usr/local/bin/jnc
 
 # Create a config directory on the installation path
 sudo mkdir -p ~/.juniper_networks/network_connect/config
-cd ~/.juniper_networks/network_connect/
+
+# Copy the user config script
+sudo cp ${SCRIPTPATH}/installer_files/uc.conf ~/.juniper_networks/network_connect/config/
 
 # Create the certificate and store it in the config path
+# Replace the user field with 6+2 id (first argument)
+cd ~/.juniper_networks/network_connect/
 sudo bash getx509certificate.sh sslvpn.uc.edu config/uc.crt
 
-# Download the user config script
-sudo wget http://homepages.uc.edu/~guptask/vpn/uc.conf \
-    -P ~/.juniper_networks/network_connect/config/
-
-# Replace the user field with 6+2 id (first argument)
 eval username="$1"
 prefix="sudo sed -i -e 's/unknown/"
 suffix="/g' ~/.juniper_networks/network_connect/config/uc.conf"
